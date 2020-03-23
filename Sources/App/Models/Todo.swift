5d49d0ec49b43e012e3066542b1b025219df6658
Artem <stratienko.artem@gmail.com>
2020-03-23 20:41:22 +0300

    make it better

--- Sources/App/Models/Todo.swift
+++ Sources/App/Models/Todo.swift
@@ -1,14 +1,28 @@
 import FluentPostgreSQL
 import Vapor
 
-final class User: PostgreSQLModel {
+
+final class User: PostgreSQLModel,Codable {
     var id: Int?
     var username: String
-    init(id: Int? = nil, username: String) {
+    var mycoordinate : Coordinate
+    
+    init(id: Int? = nil, username: String, mycoordinate : Coordinate ) {
         self.id = id
         self.username = username
+        self.mycoordinate = mycoordinate
     }
 }
+final class Coordinate:Codable {
+     let lon, lat: String
+
+       init(lon: String, lat: String) {
+           self.lon = lon
+           self.lat = lat
+       }
+}
+
+
 extension User: Content {}
 extension User: Migration {}
 extension User: Parameter {}
