b09c0111327a1dbc8f8c1544c636b251137512d2
artIU7 <stratienko.artem@gmail.com>
2020-03-31 13:14:34 +0300

    new change

--- Sources/App/Controllers/TodoController.swift
+++ Sources/App/Controllers/TodoController.swift
@@ -27,25 +27,15 @@ final class UserController {
     // view with users
     func list(_ req: Request) throws -> Future<[User]> {
             return User.query(on: req)
-             .join(\Coordinate.id,to: \User.positionID)
-             .filter(\User.positionID >= 2)
-             //.alsoDecode(User.self)
-             .all()
-        //let booking = Booking.query(on: req).join(\Booking.product, to:\Product.id).filter(\.user_id == userID).all()
-
-        /*Point.query(on: req)
-       .join(field: \OrderPoint.pointID)
-       .filter(OrderPoint.self, \OrderPoint.orderID == order.id!)
-       .all()*/
-        /*let joinData = req.withPooledConnection(to: .psql) { (conn) -> Future<[User]> in
-            conn.raw("SELECT DISTINCT ON(<my columns>) * FROM <my_table> INNER JOIN <another_table> ON <some_condition> WHERE <conditions>")
-                .all(decoding: User.self) 
-        }*/
-            //.join(\User.position, to: \Coordinate.id)
-        
-        //.all()
-        
-        // comment
+            .all()
+    }
+    func list1(_ req: Request) throws -> Future<[User]> {
+               let users =  User.query(on: req)
+                .join(\Coordinate.id,to: \User.positionID)
+                .filter(\User.positionID >= 2)
+                .all()
+        print(users)
+        return users
     }
     func list2(_ req: Request) throws -> Future<[User]> {
                let users =  User.query(on: req)
