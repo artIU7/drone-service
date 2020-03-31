e01e75e6376ef954a1321a76cf136ac0d58deb25
artIU7 <stratienko.artem@gmail.com>
2020-03-31 13:24:55 +0300

    new change

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -5,6 +5,7 @@ public func routes(_ router: Router) throws {
 
     let userController = UserController()
     router.get("users", use: userController.list)
+    router.get("users1", use: userController.list1)
     router.get("users2",use: userController.list2)
     router.get("users3",use: userController.list3)
     router.post("users", use: userController.create)
