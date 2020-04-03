0b9a72465e01e7559891492f78ce3acec44cc41a
artIU7 <stratienko.artem@gmail.com>
2020-04-03 23:00:26 +0300

    new change

--- Public/scripts/app.js
+++ Public/scripts/app.js
@@ -1,1 +1,9 @@
 alert("Hello, alert!");
+const Http = new XMLHttpRequest();
+const url='https://jsonplaceholder.typicode.com/posts';
+Http.open("GET", url);
+Http.send();
+
+Http.onreadystatechange = (e) => {
+  console.log(Http.responseText)
+}
