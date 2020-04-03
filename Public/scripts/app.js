2d382b70851cb5aec922b601e978fb307c10dd81
artIU7 <stratienko.artem@gmail.com>
2020-04-04 01:09:50 +0300

    new change

--- Public/scripts/app.js
+++ Public/scripts/app.js
@@ -1,20 +1,20 @@
 alert("Hello, alert!");
 
-
-let user = {
-    altitude: 43,
-    username: 'Smith',
-    positionID: 1
-  };
+async function postDB() {
+    let user = {
+        altitude: 43,
+        username: 'Smith',
+        positionID: 1
+    };
   
-  let response =  fetch('https://droneservice.herokuapp.com/users/', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8'
-    },
-    body: JSON.stringify(user)
-  });
+      let response =  await fetch('https://droneservice.herokuapp.com/users/', {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(user)
+     });
   
-  let result =  response.json();
-  alert(result.message);
-  console.log(JSON.parse(JSON.stringify(user)));
+      let result =  await response.json();
+    alert(result.message);
+}
