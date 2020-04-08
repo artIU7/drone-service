97fbf66d2a6cb77069e77e8a45f2ddbd42bbf9e3
Artem <stratienko.artem@gmail.com>
2020-04-08 21:41:15 +0300

    new change

--- Public/scripts/app.js
+++ Public/scripts/app.js
@@ -36,37 +36,26 @@ async function getLocation() {
     localBra = {lat:locationBrazil.lat, lng:locationBrazil.lon}
     console.log(localBra);
 }
-function geoFindMe() {
 
-    const status = document.querySelector('#status');
-    const mapLink = document.querySelector('#map-link');
-  
-    mapLink.href = '';
-    mapLink.textContent = '';
-  
     function success(position) {
+      const mapLink = document.querySelector('#map-link');
       const latitude  = position.coords.latitude;
       const longitude = position.coords.longitude;
-  
-      status.textContent = '';
+      const accuracy = position.coords.accuracy;
       mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-      mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
     }
-  
-    function error() {
-      status.textContent = 'Unable to retrieve your location';
-    }
-  
-    if (!navigator.geolocation) {
-      status.textContent = 'Geolocation is not supported by your browser';
-    } else {
-      status.textContent = 'Locating…';
-      navigator.geolocation.getCurrentPosition(success, error);
-    }
-  
+    
+  function getLocate() {
+    navigator.geolocation.getCurrentPosition(success);
   }
   
-  document.querySelector('#find-me').addEventListener('click', geoFindMe);
+    //if (!navigator.geolocation) {
+     // status.textContent = 'Geolocation is not supported by your browser';
+   // } else {
+     // status.textContent = 'Locating…';
+    //  navigator.geolocation.getCurrentPosition(success, error);
+    //} 
+  
 //
 
   //
